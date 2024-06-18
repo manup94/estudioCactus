34bacb8b9b0eb8bdded5ba45c963545bc455065c
ManuelPerezPrado <manuel.perez@asafedigital.com>
2024-06-18 15:47:13 +0200

    add skeleton and suspense for improve load timing

--- src/app/components/material-selector.tsx
+++ src/app/components/material-selector.tsx
@@ -3,6 +3,8 @@ import Image from "next/image"
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu"
 import { setSelectionSelector, useStore } from "@/app/utils/store"
 import clsx from "clsx"
+import { Suspense } from "react"
+import Skeleton from "./skeleton"
 
 interface MaterialSelectorProps {
   materials: DocumentData[]
@@ -21,27 +23,29 @@ const MaterialSelector: React.FC<MaterialSelectorProps> = ({
   return (
     <div className="h-2/3 flex flex-col gap-y-3">
       {materials?.map((material: DocumentData) => (
-        <DropdownMenu.Item key={material.id} className="outline-white">
-          <button
-            onClick={() => handleClick(material.id)}
-            className={clsx(
-              "w-full p-2 flex gap-2 items-center justify-between rounded-md",
-              selection?.[pointId] === material.id
-                ? "bg-white/80"
-                : "bg-white/20"
-            )}
-          >
-            <p className="text-[#9e9c9b] font-semibold">{material.name}</p>
-            <Image
-              priority
-              src={material.materialPreview}
-              alt={material.name}
-              width={100}
-              height={100}
-              className="max-w-[100px] max-h-[100px] rounded-md"
-            />
-          </button>
-        </DropdownMenu.Item>
+        <Suspense key={material.id} fallback={<Skeleton />}>
+          <DropdownMenu.Item className="outline-white">
+            <button
+              onClick={() => handleClick(material.id)}
+              className={clsx(
+                "w-full p-2 flex gap-2 items-center justify-between rounded-md",
+                selection?.[pointId] === material.id
+                  ? "bg-white/80"
+                  : "bg-white/20"
+              )}
+            >
+              <p className="text-[#9e9c9b] font-semibold">{material.name}</p>
+              <Image
+                priority
+                src={material.materialPreview}
+                alt={material.name}
+                width={100}
+                height={100}
+                className="max-w-[100px] max-h-[100px] rounded-md"
+              />
+            </button>
+          </DropdownMenu.Item>
+        </Suspense>
       ))}
     </div>
   )
